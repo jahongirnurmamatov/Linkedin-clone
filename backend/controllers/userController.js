36deebf8066f0f131343c8c485afb56d03b0bc3c
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-11-01 19:06:23 +0500

    updateProfile api (without photos so far)

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,22 +9,57 @@ export const getSugestedConnection = async (req, res) => {
     })
       .select("name username profilePicture headline")
       .limit(3);
-      res.status(200).json(suggestedUsers);
+    res.status(200).json(suggestedUsers);
   } catch (error) {
-    console.log(error); 
+    console.log(error);
     res.status(500).json({ error: "Internal server error!" });
   }
 };
 
-export const getPublicProfile=async(req,res)=>{
-    try {
-        const user = await User.findOne({username:req.params.username}).select("-password");
-        if(!user){
-            return res.status(404).json({message:"User not found!"});
-        }
-        res.status(200).json(user);
-    } catch (error) {
-        console.log(error); 
-        res.status(500).json({ error: "Internal server error!" });
+export const getPublicProfile = async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.params.username }).select(
+      "-password"
+    );
+    if (!user) {
+      return res.status(404).json({ message: "User not found!" });
     }
-}
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error!" });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const allowedFields = [
+      "name",
+      "headline",
+      "username",
+      "about",
+      "location",
+      "profilePicture",
+      "website",
+      "skills",
+      "education",
+      "experience",
+      "bannerImg",
+    ];
+    const updatedData = {};
+    for(const field of allowedFields){
+        if(req.body[field]){
+            updatedData[field] = req.body[field];
+        }
+    }
+    // todo check for the profile img and banner img and upload to cloudinary
+    const user = await User.findByIdAndUpdate(req.user._id,{
+        $set:updatedData
+    },{new:true});
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.log('Erron in updating profile controller',error);
+    res.status(500).json({ error: "Internal server error!" });
+  }
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { protectRoute } from '../middleware/auth.middleware.js';
-import { getPublicProfile, getSugestedConnection } from '../controllers/userController.js';
+import { getPublicProfile, getSugestedConnection, updateProfile } from '../controllers/userController.js';
 
 const userRoutes = express.Router();
 
 userRoutes.get('/suggestion',protectRoute,getSugestedConnection);
 userRoutes.get('/:username',protectRoute,getPublicProfile);
+
+userRoutes.put('/profile',protectRoute,updateProfile);
 
 export default userRoutes;
