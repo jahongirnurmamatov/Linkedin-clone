2ee3b790d30438d27669a337495f00ff94145bf0
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-11-01 20:10:16 +0500

    getFeedPosts, createPost, deletePost endpoints

---
+++ backend/controllers/postController.js
@@ -0,0 +1,69 @@
+import Post from "../models/Post.js";
+import { v2 as cloudinary } from "cloudinary";
+
+export const getFeedPosts = async (req, res) => {
+  try {
+    const posts = await Post.find({
+      author: { $in: req.user.connections },
+    })
+      .populate("author", "name username profilePicture headline")
+      .populate("comments.user", "name profilePicture")
+      .sort({ createdAt: -1 });
+
+    res.status(200).json(posts);
+  } catch (error) {
+    console.error("Error in getFeedPosts: ", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const createPost = async (req, res) => {
+  try {
+    const { content, image } = req.body;
+    let newPost;
+    if (image) {
+      const result = await cloudinary.uploader.upload(image);
+      newPost = new Post({
+        content,
+        image: result.secure_url,
+        author: req.user._id,
+      });
+    } else {
+      newPost = new Post({ content, author: req.user._id });
+    }
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    console.error("Error in createPost: ", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const postId = req.params.id;
+
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found!" });
+    }
+    // check if the current user is an author of the post
+    if (post.author.toString() !== userId.toString()) {
+      return res
+        .status(403)
+        .json({ message: "You are not the owner of the post" });
+    }
+    // todo remove image from cloudinary
+    if (post.image) {
+      await cloudinary.uploader.destroy(
+        post.image.split("/").pop().split(".")[0]
+      );
+    }
+    await Post.findByIdAndDelete(postId);
+    res.status(200).json({ message: "Post has been deleted successfully!" });
+  } catch (error) {
+    console.error("Error in deleting post: ", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import { protectRoute } from '../middleware/auth.middleware.js';
+import { createPost, getFeedPosts,deletePost } from '../controllers/postController.js';
 
 const postRoutes = express.Router();
 
-
+postRoutes.get('/',protectRoute,getFeedPosts);
+postRoutes.post('/create',protectRoute,createPost);
+postRoutes.delete('/delete/:id',protectRoute,deletePost);
 
 export default postRoutes;
