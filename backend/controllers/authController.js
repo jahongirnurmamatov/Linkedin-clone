36f510f44918e6aea0f51cfa57efd1aba89b903d
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-11-01 18:07:43 +0500

    login and logout functions

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,54 +1,85 @@
 import User from "../models/User.js";
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 
-export const signup = async(req,res)=>{
-    try {
-        const {name,email,username,password} = req.body;
-        const existingEmail = await User.findOne({email});
-        if(existingEmail){
-            return res.status(400).json({message:"Email already exists"});
-        }
-        const existingUsername = await User.findOne({username});
-        if(existingUsername){
-            return res.status(400).json({message:"Username already exists"});
-        }
-        if(password.length < 6){
-            return res.status(400).json({message:"Password should be atleast 6 characters"});
-        }
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password,salt);
-        const user = new User({name,email,username,password:hashedPassword});
-        await user.save();
-        
-        const token = jwt.sign({userId:user._id},process.env.JWT_SECRET,{
-            expiresIn:"3d"
-        });
-
-        res.cookie("jwt-linkedin",token,{
-            httpOnly:true, //prevents javascript from accessing the cookie
-            maxAge:3*24*60*60*1000,
-            sameSite:"none", //prevents cookie from being sent with cross-site requests
-            secure: process.env.NODE_ENV==='production' //sets the cookie to be sent only over HTTPS
-        }).status(201).json({message:"User created successfully"});
-
-        const profileUrl = process.env.CLIENT_URL + '/profile/' + user.username;
-        // to do send welcome email using mailtrap
-        try {
-            await sendWelcomeEmail(user.email,user.name,profileUrl);
-        } catch (error) {
-            console.error(`Error sending in welcome Email: ${error.message}	`);
-        }
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({message:"Something went wrong"});
+export const signup = async (req, res) => {
+  try {
+    const { name, email, username, password } = req.body;
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ message: "Email already exists" });
     }
-}
+    const existingUsername = await User.findOne({ username });
+    if (existingUsername) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password should be atleast 6 characters" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = new User({ name, email, username, password: hashedPassword });
+    await user.save();
 
-export const login = async(req,res)=>{
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "3d",
+    });
 
-}
-export const logout = async(req,res)=>{
+    res
+      .cookie("jwt-linkedin", token, {
+        httpOnly: true, //prevents javascript from accessing the cookie
+        maxAge: 3 * 24 * 60 * 60 * 1000,
+        sameSite: "none", //prevents cookie from being sent with cross-site requests
+        secure: process.env.NODE_ENV === "production", //sets the cookie to be sent only over HTTPS
+      })
+      .status(201)
+      .json({ message: "User created successfully" });
 
-}
+    const profileUrl = process.env.CLIENT_URL + "/profile/" + user.username;
+    // to do send welcome email using mailtrap
+    try {
+      await sendWelcomeEmail(user.email, user.name, profileUrl);
+    } catch (error) {
+      console.error(`Error sending in welcome Email: ${error.message}	`);
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "3d",
+    });
+    res
+      .cookie("jwt-linkedin", token, {
+        httpOnly: true,
+        maxAge: 3 * 24 * 60 * 60 * 1000,
+        sameSite: "none",
+        secure: process.env.NODE_ENV === "production",
+      })
+      .status(200)
+      .json({ message: "User logged in successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+export const logout = async (req, res) => {
+  res.clearCookie("jwt-linkedin");
+  res.status(200).json({ message: "User logged out successfully" });
+};
