751797613bfa53227c6f5a2687f08b6221ea59a8
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-11-01 19:51:16 +0500

    Post and notification models

---
+++ backend/routes/postRoutes.js
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const postRoutes = express.Router();
+
+
+
+export default postRoutes;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import authRoutes from './routes/authRoute.js';
-import userRoutes from './routes/userRoutes.js';
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoute.js";
+import userRoutes from "./routes/userRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
 
-import { connectDb } from './lib/db.js';
-import cookieParser from 'cookie-parser';
+import { connectDb } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -14,11 +15,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/v1/auth',authRoutes);
-app.use('/api/v1/user',userRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/posts", postRoutes);
 
-
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);
-    connectDb();
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDb();
+});
