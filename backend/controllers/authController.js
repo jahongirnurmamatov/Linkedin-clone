bf8626e4ca6fbaa55b6548c878bcea9e1fd1fce4
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-11-03 19:30:57 +0500

    Login page and login functionality

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,67 +4,71 @@ import jwt from "jsonwebtoken";
 import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 
 export const signup = async (req, res) => {
-	try {
-		const { name, username, email, password } = req.body;
+  try {
+    const { name, username, email, password } = req.body;
 
-		if (!name || !username || !email || !password) {
-			return res.status(400).json({ message: "All fields are required" });
-		}
-		const existingEmail = await User.findOne({ email });
-		if (existingEmail) {
-			return res.status(400).json({ message: "Email already exists" });
-		}
+    if (!name || !username || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
 
-		const existingUsername = await User.findOne({ username });
-		if (existingUsername) {
-			return res.status(400).json({ message: "Username already exists" });
-		}
+    const existingUsername = await User.findOne({ username });
+    if (existingUsername) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
 
-		if (password.length < 6) {
-			return res.status(400).json({ message: "Password must be at least 6 characters" });
-		}
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
 
-		const salt = await bcrypt.genSalt(10);
-		const hashedPassword = await bcrypt.hash(password, salt);
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-		const user = new User({
-			name,
-			email,
-			password: hashedPassword,
-			username,
-		});
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+      username,
+    });
 
-		await user.save();
+    await user.save();
 
-		const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "3d" });
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "3d",
+    });
 
-		res.cookie("jwt-linkedin", token, {
-			httpOnly: true, // prevent XSS attack
-			maxAge: 3 * 24 * 60 * 60 * 1000,
-			sameSite: "strict", // prevent CSRF attacks,
-			secure: process.env.NODE_ENV === "production", // prevents man-in-the-middle attacks
-		});
+    res.cookie("jwt-linkedin", token, {
+      httpOnly: true, // prevent XSS attack
+      maxAge: 3 * 24 * 60 * 60 * 1000,
+      sameSite: "strict", // prevent CSRF attacks,
+      secure: process.env.NODE_ENV === "production", // prevents man-in-the-middle attacks
+    });
 
-		res.status(201).json({ message: "User registered successfully" });
+    res.status(201).json({ message: "User registered successfully" });
 
-		const profileUrl = process.env.CLIENT_URL + "/profile/" + user.username;
+    const profileUrl = process.env.CLIENT_URL + "/profile/" + user.username;
 
-		try {
-			await sendWelcomeEmail(user.email, user.name, profileUrl);
-		} catch (emailError) {
-			console.error("Error sending welcome Email", emailError);
-		}
-	} catch (error) {
-		console.log("Error in signup: ", error.message);
-		res.status(500).json({ message: "Internal server error" });
-	}
+    try {
+      await sendWelcomeEmail(user.email, user.name, profileUrl);
+    } catch (emailError) {
+      console.error("Error sending welcome Email", emailError);
+    }
+  } catch (error) {
+    console.log("Error in signup: ", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
-
 
 export const login = async (req, res) => {
   try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    console.log(user);
     if (!user) {
       return res.status(400).json({ message: "User not found" });
     }
@@ -75,15 +79,13 @@ export const login = async (req, res) => {
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
       expiresIn: "3d",
     });
-    res
-      .cookie("jwt-linkedin", token, {
-        httpOnly: true,
-        maxAge: 3 * 24 * 60 * 60 * 1000,
-        sameSite: "none",
-        secure: process.env.NODE_ENV === "production",
-      })
-      .status(200)
-      .json({ message: "User logged in successfully" });
+    res.cookie("jwt-linkedin", token, {
+      httpOnly: true, // prevent XSS attack
+      maxAge: 3 * 24 * 60 * 60 * 1000,
+      sameSite: "strict", // prevent CSRF attacks,
+      secure: process.env.NODE_ENV === "production", // prevents man-in-the-middle attacks
+    });
+    res.status(200).json({ message: "User logged in successfully" });
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ message: "Something went wrong" });
