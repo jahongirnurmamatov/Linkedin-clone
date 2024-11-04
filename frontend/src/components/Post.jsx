b5932700924e409ab87855730fae7238c825c494
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-11-04 20:55:15 +0500

    post comment functionality

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -3,7 +3,15 @@ import React, { useState } from "react";
 import { axiosInstanse } from "../lib/axios";
 import toast from "react-hot-toast";
 import { Link } from "react-router-dom";
-import { Loader, MessageCircle, Share2, ThumbsUp, Trash2 } from "lucide-react";
+import { formatDistanceToNow } from "date-fns";
+import {
+  Loader,
+  MessageCircle,
+  Send,
+  Share2,
+  ThumbsUp,
+  Trash2,
+} from "lucide-react";
 import PostAction from "./PostAction";
 
 const Post = ({ post }) => {
@@ -30,8 +38,10 @@ const Post = ({ post }) => {
   });
 
   const { mutate: createComment, isPending: isCreatingComment } = useMutation({
-    mutationFn: async (comment) => {
-      await axiosInstanse.post(`/posts/${post._id}/comment`, comment);
+    mutationFn: async (newComment) => {
+      await axiosInstanse.post(`/posts/${post._id}/comment`, {
+        content: newComment,
+      });
     },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["posts"] });
@@ -59,11 +69,32 @@ const Post = ({ post }) => {
       return deletePost(post._id);
     }
   };
-  const handleLikePost = async() => {
-    if(isLikingPost){return}
+  const handleLikePost = async () => {
+    if (isLikingPost) {
+      return;
+    }
     likePost();
   };
 
+  const handleAddComment = async (e) => {
+    e.preventDefault();
+    if (newComment.trim()) {
+      createComment(newComment);
+      setNewComment("");
+      setComments([
+        ...post.comments,
+        {
+          content: newComment,
+          user: {
+            _id: authUser._id,
+            name: authUser.name,
+            profilePicture: authUser.profilePicture,
+          },
+          createdAt: new Date(),
+        },
+      ]);
+    }
+  };
   return (
     <div className="bg-secondary rounded-lg shadow mb-4">
       <div className="p-4">
@@ -79,7 +110,12 @@ const Post = ({ post }) => {
             />
             <Link to={`/profile/${post?.author?.username}`}>
               <h3 className="font-semibold">{post?.author.name}</h3>
-              <p className="text-gray-500 font-xs">Linkedin user</p>
+              <p className="text-info font-xs">{post?.author.headline}</p>
+              <p className="text-info font-xs">
+                {formatDistanceToNow(new Date(post.createdAt), {
+                  addSuffix: true,
+                })}
+              </p>
             </Link>
           </div>
           {isOwner && (
@@ -122,6 +158,56 @@ const Post = ({ post }) => {
           <PostAction icon={<Share2 size={18} />} text={`Share`} />
         </div>
       </div>
+      {showComment && (
+        <div className="px-4 pb-4">
+          <div className="mb-4 max-h-60 overflow-y-auto">
+            {comments.map((comment) => (
+              <div
+                key={comment._id}
+                className="mb-2 bg-base-100 p-2 rounded flex items-start"
+              >
+                <img
+                  src={comment.user.profilePicture || "/avatar.png"}
+                  alt={comment.user.name}
+                  className="size-8 rounded-full mr-2 flex-shrink-0"
+                />
+
+                <div className="flex-grow">
+                  <div className="flex items-center mb-1">
+                    <span className="font-semibold mr-2">
+                      {comment.user.name}
+                      <span className="ml-2 text-info text-xs">
+                        {formatDistanceToNow(new Date(comment.createdAt))}
+                      </span>
+                    </span>
+                    <p>{comment.content}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <form className="flex items-center " onSubmit={handleAddComment}>
+            <input
+              type="text"
+              placeholder="Add a comment ..."
+              className="flex-grow p-2 rounded-l-full bg-base-100 focus:outline-none focus:ring-2 focus:ring-primary"
+              value={newComment}
+              onChange={(e) => setNewComment(e.target.value)}
+            />
+            <button
+              disabled={isCreatingComment}
+              type="submit"
+              className="bg-primary text-white p-2 rounded-r-full hover:bg-primary-dark transition duration-300"
+            >
+              {isCreatingComment ? (
+                <Loader size={18} className="animate-spin" />
+              ) : (
+                <Send size={18} />
+              )}
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
